a992fc4428e7cd73bee83e42ad6da069a3010956
valeriasb05 <sierravaleria815@gmail.com>
2025-05-13 21:05:15 -0500

    tema claro predeterminado y bloquedo de giro de pantalla

--- app/src/main/java/com/example/calmaapp_proyect/ui/theme/Theme.kt
+++ app/src/main/java/com/example/calmaapp_proyect/ui/theme/Theme.kt
@@ -9,7 +9,12 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -35,23 +40,30 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun Calmaapp_proyectTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false, // Forzamos el tema claro estableciendo darkTheme siempre a false
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-        val context = LocalContext.current
-        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      darkTheme -> DarkColorScheme
-      else -> LightColorScheme
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            dynamicLightColorScheme(context) // Siempre usamos el tema claro dinámico si está disponible
+        }
+        else -> LightColorScheme // En caso contrario, usamos nuestro tema claro predefinido
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false // Forzamos iconos oscuros en la barra de estado
+        }
     }
 
     MaterialTheme(
-      colorScheme = colorScheme,
-      typography = Typography,
-      content = content
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
     )
 }
